758f2268fdd77ba19f498a06dcfc937bb52f8528
unknown <ailumacchia@hotmail.com>
2022-06-20 10:58:22 -0300

    first commit

---
+++ Desktop/PI-Countries-main/PI-Countries-main/api/src/models/Activity.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('country', {
+
+  }, {
+    timestamps: false
+  });
+};
+
+
+// ID
+// Nombre
+// Dificultad (Entre 1 y 5)
+// Duración
+// Temporada (Verano, Otoño, Invierno o Primavera)
